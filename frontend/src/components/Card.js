0196bebc10e8b31b941750e68f7cde45a5c702c4
github username <github email address>
2019-12-17 15:37:45 -0700

    Final version.

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -8,13 +8,14 @@ class Card extends Component{
 
     render(){
         const {isClicked} = this.state
-        const {name, description, photo, location, danger_level} = this.props.animal
+        const {name, description, photo, location, danger_level, picture} = this.props.animal
         return(
             <>
-                <img onClick={() => this.setState({isClicked: !isClicked})} src={photo} className='picture'/>
+                <img onClick={() => this.setState({isClicked: !isClicked})} src={picture} className='picture'/>
                 {isClicked ? (
                 <div className="info">
                     <h1 className="name">{name}</h1>
+                    <img className="photo" src={photo}/>
                     <p className="description">{description}</p>
                     <h4 className="danger-level">Danger level: {danger_level}</h4>
                     <p className="location">{location}</p>
